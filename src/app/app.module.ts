ab39db05b558754233d3bf7834601e364160d1f2
zhengfeirui <zhengfeirui@gmail.com>
2022-09-01 20:03:01 -0700

    add 2 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ArchivesComponent } from './pages/archives/archives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ArchivesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'aboutus', component: AboutUsComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
